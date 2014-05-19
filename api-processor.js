4cb1f21c7e3511b701c685356f719576717ab5e9
Jan Mlcoch <joshuatrii@Jans-MacBook-Pro.local>
2014-05-19 17:12:59 -0400

    work on import frame

--- api-processor.js
+++ api-processor.js
@@ -1,7 +1,6 @@
 var commands = require('commander');
 var request = require('request');
 
-
 var config;
 
 commands
@@ -16,34 +15,101 @@ if(!commands.config){
 else{
 
 	config = require('./' + commands.config);
+
+	var tasks = [];
+
+	processAPIs(config);
 }
 
 
-processAPIs(config);
-
+var global = [];
 
 
 function processAPIs(config){
 
 
-	//just for testing now, but will need to make some requests in order 
-	for(var i=0; i < config.length; i++){
-		
-		request(config[i].options, callback);
+var APIs = config;
+var APIs = APIs.map(function (api) {
+  return api;
+});
 
-		//console.log(config[i].options);
+function callAPIs (APIs, data) {
+  var API = APIs.shift();
+
+  	if(API.get_data){
+  		getValue(API.get_data.get, data);
+  		setValue(API.get_data.set, API.options, getValue(API.get_data.get, data));
+  	}
+
+  	if(API.set_from_global) {
+  		setFromGlobal(API.set_from_global, API.options);
+  	}
+
+  request(API.options, function(err, res, body) { 
+  	console.log(body);
+
+  	body = JSON.parse(body);
+    if( APIs.length ) {
+
+    	if(API.set_to_global) {
+    		setToGlobal(API.set_to_global, body);
+    	}
+
+      callAPIs (APIs, body);
+    }
+  });
+}
+
+callAPIs(APIs);
+
+}
+
+function getValue(path, data) {
+
+	
+	var keys = path.split(".");
+
+	for (var i = 0; i < keys.length; i++){
+		data = data[keys[i]];
 	}
 
-
+	return data;
 }
 
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body);
-        console.log(info);
+
+function setValue(path, options, value) {
+
+	var keys = path.split(".");
+	var opValue = options;
+
+	for (var i = 0; i < keys.length -1; i++) {
+		opValue = opValue[keys[i]];
+	}
+
+	opValue[keys[keys.length - 1]] = value;
+}
+
+function setToGlobal(options, data) {
+
+
+
+	for (var key in options) {
+
+        global[key] =  getValue(options[key], data);
     }
+
+    //console.log(options);
 }
 
+function setFromGlobal(options, data) {
 
 
- 
+
+	for (var key in options) {
+
+        setValue(options[key], data, global[key]);
+    }
+
+    //console.log(global);
+}
+
