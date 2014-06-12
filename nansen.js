57ced27d025cc6ef1887c3e204b773ea1daff246
Jan Mlcoch <joshuatrii@trc106.apng.wireless-pennnet.upenn.edu>
2014-06-12 16:26:16 -0400

    wrap module

--- nansen.js
+++ nansen.js
@@ -1,96 +1,107 @@
-var commands = require('commander');
 var request = require('request');
-var bunyan = require('bunyan');
 
-var log = bunyan.createLogger({name: "nansen"});
-var config;
 var global_params = [];
-
-commands
-  .version('0.0.0')
-  .option('-o, --config [filename]', 'Process api config from config file [filename]')
-  .parse(process.argv);
-
-if(!commands.config){
-	console.log('nansen requires a config to process APIs, see --help.');
-
-}
-else{
-
-	config = require('./' + commands.config);
-
-	var tasks = [];
-
-	processAPIs(config);
-}
+var responce_data = [];
 
 
-log.info("nansen has started.");
+module.exports = {
+   /**
+   * Processes a list of APIs for the given options
+   *
+   * @param  {obj} options
+   * @param  {function} callback
+   */
+  processAPIs: function(options, callback) {
 
-function processAPIs(config) {
+	infoMessage = "Starting to process APIs";
+	callback(infoMessage);
 
+	var errorMessage;
+	var infoMessage;
 
-var APIs = config;
-var APIs = APIs.map(function (api) {
-  return api;
-});
+	var APIs = options;
+	var APIs = APIs.map(function (api) {
+	  return api;
+	});
 
-function callAPIs (APIs, data) {
-	  
-	  var API = APIs.shift();
+	function callAPIs (APIs, data) {
+		  
+		  var API = APIs.shift();
 
-	  if(API.set_params_from_previous) {
-	  	getValue(API.set_params_from_previous, data);
-	  	setValue(API.set_params_from_previous, API.options, getValue(API.set_params_from_previous.get, data));
-	  }
+		  if(API.set_params_from_previous) {
+		  	getValue(API.set_params_from_previous, data);
+		  	setValue(API.set_params_from_previous, API.options, getValue(API.set_params_from_previous.get, data));
+		  }
 
-	  if(API.set_rest_params_from_previous) {
-	  	setRestParamsFromPrevious(API.set_rest_params_from_previous, API.options, data);
-	  }
+		  if(API.set_rest_params_from_previous) {
+		  	setRestParamsFromPrevious(API.set_rest_params_from_previous, API.options, data);
+		  }
 
-	  if(API.set_params_from_global) {
-	  	setFromGlobal(API.set_params_from_global, API.options);
-	  }
+		  if(API.set_params_from_global) {
+		  	setFromGlobal(API.set_params_from_global, API.options);
+		  }
 
-	  if(API.set_rest_params_from_global) {
-	  	setRestParamsFromGlobal(API.set_rest_params_from_global, API.options);
-	  }
+		  if(API.set_rest_params_from_global) {
+		  	setRestParamsFromGlobal(API.set_rest_params_from_global, API.options);
+		  }
 
-	  if(API.sub_process) { 
-	  	//console.log("entering sub_process");
-	  	//console.log(APIs);
+		  request(API.options, function(err, res, body) { 
+		  	
+		  	if(!err)	{
 
-	  	APIs = API.sub_process.concat(APIs);
-	  	//console.log(APIs);
-	  	//processAPIs(API.sub_process);
-	  }
+		  		infoMessage = "Success processing: " + API.options.url;
+		  		callback(infoMessage);
 
-	  request(API.options, function(err, res, body) { 
-	  	
-	  	if(!err)	{
+		  		if (res){
+	    			try{
+	        			body = JSON.parse(body);
+	        			responce_data.push(body);	
+	    			}
+	    			catch(e){
+	        			errorMessage = "Encountered an error attempting to parse responce from: " + API.options.url + " Error :" + e;
+						callback(errorMessage);
+	    			}			
+				}else{
+					errorMessage = "Got an empty responce from: " + API.options.url;
+					callback(errorMessage);
+				}
+		    	
+		    	if(APIs.length) {
 
-	  		//for testing
-	  		console.log(API.options.url);
-	  		console.log("success");
-	  		//console.log(body);
-	  		//
-	  	
-	  		body = JSON.parse(body);
-	    	
-	    	if( APIs.length ) {
+		    		if(API.save_params_to_global) {
+		    			saveToGlobal(API.save_params_to_global, body);
+		    		}
 
-	    		if(API.save_params_to_global) {
-	    			saveToGlobal(API.save_params_to_global, body);
-	    		}
+		    		if(API.sub_process) { 
+		  				infoMessage = "Adding subprocess of " + API.sub_process.length + " APIs to process queue";
+		  				callback(infoMessage);
+		  				APIs = API.sub_process.concat(APIs);
+		  			}
 
-	      		callAPIs (APIs, body);
-	    	}
+		  			if(data) {
+		  				if(data.sub_process){
+		  					infoMessage = "Adding subprocess of " + data.sub_process.length + " APIs to process queue";
+		  					callback(infoMessage);
+		  					APIs = data.sub_process.concat(APIs);
+		  				}
+		  			}
+
+		      		callAPIs (APIs, body);
+		    	}
+		    	else {
+		    		callback(null, responce_data);
+		    	}
+			}
+			else{
+				errorMessage = "encountered an error attempting to process " + API.options.url + "Error :" + err;
+				callback(errorMessage);
+			}
+		  });
 		}
-	  });
-	}
 
-	callAPIs(APIs);
-}
+		callAPIs(APIs);
+	}
+};
 
 function getValue(path, data) {
 	
@@ -117,42 +128,28 @@ function setValue(path, options, value) {
 
 function saveToGlobal(options, data) {
 
-
-
 	for (var key in options) {
-
         global_params[key] =  getValue(options[key], data);
     }
-
-    //console.log("saved params to global " + getValue(options[key], data));
 }
 
 function setFromGlobal(options, data) {
 
-
-
 	for (var key in options) {
-
         setValue(options[key], data, global_params[key]);
     }
-
-    //console.log(global_params);
 }
 
 function setRestParamsFromPrevious(restParams, options, data) {
 
-      for (var key in restParams) {
-
-        options.url =  options.url.replace(key,  getValue(restParams[key], data));
+    for (var key in restParams) {
+		options.url =  options.url.replace(key,  getValue(restParams[key], data));
     }
-    //    console.log(options.url);
 }
 
 function setRestParamsFromGlobal(restParams, options) {
 
       for (var key in restParams) {
-
-        options.url =  options.url.replace(key, global_params[restParams[key]]);
+		options.url =  options.url.replace(key, global_params[restParams[key]]);
     }
-        //console.log(global_params);
 }
