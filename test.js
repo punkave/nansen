80edf461b3e913c0f29a02c37fd111163727c5cf
mcoppola <mcoppola832@gmail.com>
2014-12-11 11:13:11 -0500

    implemented validate function and array support, improved logging

--- test.js
+++ test.js
@@ -15,13 +15,23 @@ var config = {
 			});
 			
 		},
-		complete: function(response, callback) {
+		validate: function(res, callback) {
 
-			response = JSON.parse(response);
+			res = JSON.parse(res);
+
+			if (res && res.paths) {
+				callback(null, res);
+			} else {
+				callback("Error: No paths in Setup response.");
+			}
+		},
+		complete: function(res, callback) {
+
+			res = JSON.parse(res);
 
 			var items = [];
 
-			response.paths.forEach(function(p, i){
+			res.paths.forEach(function(p, i){
 				items.push({ "path": p.path });
 			});
 
@@ -32,13 +42,14 @@ var config = {
 
 	get: {
 
-		request: function(data, callback ){
+		request: function(data, callback ) {
 
 			callback(null, {
 				method: 'GET',
 				uri: 'http://demo0841709.mockable.io/' + data.path
 			});
-		}
+		},
+		validate: ['json']
 	},
 
 	post: {
